88b554f15b0dcd171f94a3c882a2a237bcf9d60a
abdullah-al-shishir <abdullahshishir2018@gmail.com>
2025-08-15 12:29:39 +0600

    catagory items  listed

--- index.js
+++ index.js
@@ -69,18 +69,72 @@ function catagorySection() {
   const catagoryContainer = document.createElement("div");
   catagoryContainer.classList.add("catagory-container");
 
-  // First div
+  // Fiction div
   const fictionDiv = document.createElement("div");
+  fictionDiv.classList.add("common-div-style");
   const fictionHeading = document.createElement("h1");
-  fictionHeading.innerText = "wh";
+  fictionHeading.classList.add("common-heading-style");
+  fictionHeading.innerText = "Fiction";
   const fictionList = document.createElement("p");
-  fictionList.innerText = "a book wegfuwf wwfbwriug wbfuwf ubuwibfw wbfwvf fw";
+  fictionList.innerHTML = `1. Create Your Own Business <br> 2. ইহযৌবন <br> 3. প্রেক্ষাপটের দাসদাসী <br> 4. জলেশ্বরী : উপন্যাস ও গল্প সংগ্রহ খণ্ড ১ <br> 5. আবুল মনসুর আহমদের শ্রেষ্ঠ গল্প (পেপারব্যাক) <br> 6. কার্ল মার্ক্সের সমাধিতে হুমায়ূন আহমেদ`;
 
   fictionDiv.appendChild(fictionHeading);
   fictionDiv.appendChild(fictionList);
 
+  // Action Div
+  const actionDiv = document.createElement("div");
+  actionDiv.classList.add("common-div-style");
+  const actionHeading = document.createElement("h1");
+  actionHeading.classList.add("common-heading-style");
+  actionHeading.innerText = "Action";
+  const actionList = document.createElement("p");
+  actionList.innerHTML = `1. Pumkin Juice <br> 2. শহীদুল জহির গল্পসমগ্র <br> 3. Until August: The Lost Novel`;
+
+  actionDiv.appendChild(actionHeading);
+  actionDiv.appendChild(actionList);
+
+  // Slice of life div
+  const sliceOfLifeDiv = document.createElement("div");
+  sliceOfLifeDiv.classList.add("common-div-style");
+  const sliceOfLifeHeading = document.createElement("h1");
+  sliceOfLifeHeading.classList.add("common-heading-style");
+  sliceOfLifeHeading.innerText = "Slice Of Life";
+  const sliceOfLifeList = document.createElement("p");
+  sliceOfLifeList.innerHTML = `1. Celebrate The Day of Forest <br> 2. আগুনের খেলা ও অন্যান্য গল্প <br> 3. চাষাঢে গল্প <br> 4. Still Me (Penguin)`;
+
+  sliceOfLifeDiv.appendChild(sliceOfLifeHeading);
+  sliceOfLifeDiv.appendChild(sliceOfLifeList);
+
+  //Non-Fiction div
+  const nonFictionDiv = document.createElement("div");
+  nonFictionDiv.classList.add("common-div-style");
+  const nonFictionHeading = document.createElement("h1");
+  nonFictionHeading.classList.add("common-heading-style");
+  nonFictionHeading.innerText = "Non-Fiction";
+  const nonFictionList = document.createElement("p");
+  nonFictionList.innerHTML = `1. Taxi Wallah and Other Stories <br> 2. The Living Mountain: A Fable For Our Times`;
+
+  nonFictionDiv.appendChild(nonFictionHeading);
+  nonFictionDiv.appendChild(nonFictionList);
+
+  // Academic div
+  const academicDiv = document.createElement("div");
+  academicDiv.classList.add("common-div-style");
+  const academicHeading = document.createElement("h1");
+  academicHeading.classList.add("common-heading-style");
+  academicHeading.innerText = "Academic";
+  const academicList = document.createElement("p");
+  academicList.innerHTML = `1. Anatomy of Silence`;
+
+  academicDiv.appendChild(academicHeading);
+  academicDiv.appendChild(academicList);
+
   // Pushing all divs in the catagoryContainer div
   catagoryContainer.appendChild(fictionDiv);
+  catagoryContainer.appendChild(actionDiv);
+  catagoryContainer.appendChild(sliceOfLifeDiv);
+  catagoryContainer.appendChild(nonFictionDiv);
+  catagoryContainer.appendChild(academicDiv);
   return catagoryContainer;
 }
 
